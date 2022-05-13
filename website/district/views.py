937ecd6ba6a898e53c2da80369f73d2e86febc15
Romuald GRIGNON <romuald.grignon@gmail.com>
2022-05-13 09:50:40 +0200

    update template for language list display

--- website/district/views.py
+++ website/district/views.py
@@ -6,15 +6,23 @@ from district.models import Language
 
 # Create your views here.
 def coding(request):
+    # Get parameters from request (e.g. : form field values)
+    # either with the GET method or the POST method
+    print(request.GET)
+    print(request.POST)
+
     # dictionary for initial data with
     # field names as keys
     context = {}
 
+    # Load view template
     template = loader.get_template('coding/languages.html')
 
+    # Create context dictionary
+    # Use any needed models for that
     context["title"] = "List of handled programming languages"
     context["languages"] = Language.objects.all()
 
-
+    # Use context in the template and render response view
     return HttpResponse(template.render(context, request))
 
