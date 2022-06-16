b43863c751695193a27f0fea44a5e4833814a33e
CYTech Student <vincent.ky09@gmail.com>
2022-06-16 17:48:29 +0200

    adding links between pages, editing controllers (changing filter or move the functional part in utils/

--- website/district/controllers/ctrl_assessment.py
+++ website/district/controllers/ctrl_assessment.py
@@ -5,6 +5,7 @@ from django.template import loader
 from django.utils import timezone
 
 from district.models.assessment import Assessment
+from toolbox.utils.assessment import get_asse_exercises
 
 from website.settings import LOGIN_URL
 
@@ -12,51 +13,13 @@ from website.settings import LOGIN_URL
 # display the list of exercises in an assessment
 @login_required(login_url=LOGIN_URL)
 def ctrl_asse_details(request, id_asse):
-    # get current user
-    curr_user = request.user
-    # get the Assessment object
-    curr_asse = Assessment.objects.filter(
-        id=id_asse,
-        start_time__lt=timezone.now(),
-        groups__userdc=curr_user
-    )
-    # only accessible assessments
-    if not curr_asse.exists():
+    result = get_asse_exercises(request, id_asse)
+    print(result)
+    if result["exit_code"] != 0:
         return HttpResponse("Access denied")
-
-    # dictionary for initial data
-    context = {"assessment": curr_asse}
 
     # Load view template
     template = loader.get_template('district/exercisesAssess.html')
 
-    # TEST
-    all_other_asse = Assessment.objects.filter(
-        ~Q(id=id_asse),
-        groups__userdc=curr_user
-    )
-
-    # let's build a dictionary  composed by all exercises of the assessment
-    exos = {}
-    for ex2test in curr_asse.first().test_id.exo2test_set.all():
-        ex = ex2test.exercise_id
-        exos[ex.id] = {"ex_obj": ex, "is_available": True, "lang_objs": []}
-        # adding languages available
-        for ex_tst_lng in ex2test.exotest2lang_set.all():
-            lang = ex_tst_lng.lang_id
-            exos[ex.id]["lang_objs"].append(lang)
-
-    # now, we set or remove elements from exos
-    for asse in all_other_asse:
-        for ex2test in asse.test_id.exo2test_set.all():
-            ex = ex2test.exercise_id
-            if ex.id in exos:
-                # if the assessment is in progress
-                if asse.start_time.__le__(timezone.now()) and timezone.now().__lt__(asse.end_time):
-                    exos[ex.id]["is_available"] = False
-                elif asse.start_time.__gt__(timezone.now()):  # if the assessment will start in the future
-                    del exos[ex.id]
-
-    context["exercises"] = exos
     # Use context in the template and render response view
-    return HttpResponse(template.render(context, request))
+    return HttpResponse(template.render(result, request))
--- website/district/controllers/ctrl_user.py
+++ website/district/controllers/ctrl_user.py
@@ -1,13 +1,11 @@
-from django.conf.global_settings import LOGIN_REDIRECT_URL
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse
 from django.shortcuts import redirect
 from django.template import loader
 
 from toolbox.users.signup import SignupForm
 from district.models.group import GroupDC
-from district.models.user import UserDC
 from website.settings import LOGIN_URL
 
 
@@ -34,31 +32,6 @@ def ctrl_user_profile(request):
     template = loader.get_template('district/user_profile.html')
 
     return HttpResponse(template.render(context, request))
-
-
-# let the user join a group
-@login_required(login_url=LOGIN_URL)
-def ctrl_user_register(request):
-    # get parameters from request
-    register_key = request.POST.get("register_key", "")
-    user_id = request.POST.get("user_id", 0)
-
-    if len(register_key) == 0 or user_id == 0:
-        return JsonResponse({"exit_code": 2})
-
-    # check if the key exists. In this case, get the id of the group concerned
-    groups = GroupDC.objects.filter(register_key=register_key)
-    if not groups.exists():
-        return JsonResponse({"exit_code": 1})
-
-    # link the group to the user
-    user_obj = UserDC.objects.get(id=user_id)
-    user_obj.groups.add(groups.first())
-    user_obj.save()
-
-    # return a dictionary
-    return JsonResponse({"exit_code": 0})
-
 
 
 def ctrl_user_signup(request):
