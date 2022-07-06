35869eece09cca731026ca81e17e0d13d188cb3d
CYTech Student <vincent.ky09@gmail.com>
2022-07-06 17:10:19 +0200

    editing urls (making constans file) + trying to make a basic test script

--- website/toolbox/utils/route_mgr.py
+++ website/toolbox/utils/route_mgr.py
@@ -1,4 +1,4 @@
-from config.constants.route_cnf import PAGES
+import config.constants.route_cnf as route_cnf
 
 
 class Page():
@@ -10,8 +10,19 @@ class Page():
         self.type = type
         self.log_req= log_req
 
+    def __str__(self):
+        return "Page", self.name
+
 
 class PageManager():
+    instance = None
+
+    @staticmethod
+    def get_instance():
+        if PageManager.instance == None:
+            PageManager.instance = PageManager()
+        return PageManager.instance
+
 
     def __check_name(self, name):
         if name not in self.pages:
@@ -19,7 +30,7 @@ class PageManager():
 
     def __init__(self):
         self.pages = {}
-        for page in PAGES:
+        for page in route_cnf.PAGES:
             if page.name in self.pages:
                 raise Exception(f"Page already added ! {page}")
             self.pages[page.name] = page
@@ -48,6 +59,15 @@ class PageManager():
         page = self.getPage(name)
         return page.ctrl
 
-    def login_required(self, name):
+    def is_login_required(self, name):
         page = self.getPage(name)
         return page.log_req
+
+    def get_pages(self):
+        return self.pages
+
+    def get_names(self):
+        names = []
+        for page in self.pages:
+            names.append(page.name)
+        return names
