225ce7e57da5818f4696b3a38a907371399b4cd3
CYTech Student <vincent.ky09@gmail.com>
2022-07-07 17:17:22 +0200

    test exercise inspector + anonymous test

--- website/config/constants/route_cnf.py
+++ website/config/constants/route_cnf.py
@@ -13,11 +13,11 @@ PAGES = [
     Page('home', '', ctrl_home, log_req=False),
 
     Page('signup', 'accounts/signup/', ctrl_user_signup, log_req=False),
-    Page('activate', 'accounts/activate/<slug:uidb64>/<slug:token>/', ctrl_user_validate_email, log_req=False),
+    Page('activate', 'accounts/activate/<slug:uidb64>/<slug:token>/', ctrl_user_validate_email, log_req=False, parameters=True),
     # Page('login', 'accounts/login/', include("django.contrib.auth.urls"), log_req=False),
     Page('profile', 'accounts/profile/', ctrl_user_profile),
     Page('update', 'accounts/update/', ctrl_user_update),
-    Page('group_register', 'accounts/group_register/', ctrl_json_user_register, type="json"),
+    Page('group_register', 'accounts/group_register/', ctrl_json_user_register, type="json", parameters=True),
     Page('my_groups', 'accounts/my_groups/', ctrl_json_user_groups, type="json"),
 
     Page('password_reset_request' , 'accounts/password_reset/', ctrl_password_reset_request, log_req=False),
@@ -25,7 +25,7 @@ PAGES = [
     #     template_name='registration/password_reset_done.html'), log_req=False),
     # Page('password_reset_confirm' , 'accounts/password_reset_confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
     #     template_name="registration/password_reset_confirm.html"), log_req=False),
-    Page('password_reset_complete','accounts/reset/done/', auth_views.PasswordResetCompleteView.as_view(
+    Page('password_reset_complete', 'accounts/reset/done/', auth_views.PasswordResetCompleteView.as_view(
         template_name='registration/reset_password_complete.html'), log_req=False),
 
     Page('password_change_done', 'accounts/password_change/done/', ctrl_password_change_done),
@@ -33,13 +33,13 @@ PAGES = [
         template_name="registration/change_password.html")),
 
     Page('email_change_auth', 'accounts/email_change_auth/', ctrl_email_change_auth),
-    Page('email_change_confirm', 'accounts/email_change_confirm/<int:user_id>/', ctrl_email_verification, log_req=False),
+    Page('email_change_confirm', 'accounts/email_change_confirm/<int:user_id>/', ctrl_email_verification, log_req=False, parameters=True),
     Page('email_change_send', 'accounts/email_change_send/', ctrl_json_sending_email, type="json", log_req=False),
 
-    Page('assessment_details', 'assessment/details/<int:id_asse>/', ctrl_asse_details),
-    Page('exercise_details', 'exercise/details/', ctrl_exercise_details),
-    Page('exercise_write', 'exercise/write/', ctrl_exercise_write),
-    Page('exercise_inspect', 'exercise/inspect/', ctrl_json_exercise_inspect, type='json'),
-    Page('exercise_stats', 'exercise/stats/', ctrl_json_testresult_exists, type='json'),
+    Page('assessment_details', 'assessment/details/<int:id_asse>/', ctrl_asse_details, parameters=True),
+    Page('exercise_details', 'exercise/details/', ctrl_exercise_details, parameters=True),
+    Page('exercise_write', 'exercise/write/', ctrl_exercise_write, parameters=True),
+    Page('exercise_inspect', 'exercise/inspect/', ctrl_json_exercise_inspect, type='json', parameters=True),
+    Page('exercise_stats', 'exercise/stats/', ctrl_json_testresult_exists, type='json', parameters=True),
 
 ]
--- website/district/tst_folder/simple/test_anonymous.py
+++ website/district/tst_folder/simple/test_anonymous.py
@@ -3,6 +3,8 @@ from django.test import TransactionTestCase, RequestFactory
 from django.urls import reverse
 
 from config.constants.route_cnf import PAGES
+from district.models.user import UserDC
+from toolbox.utils.route_mgr import PageManager
 
 
 class AnonymousTest(TransactionTestCase):
@@ -15,19 +17,48 @@ class AnonymousTest(TransactionTestCase):
         # Every test needs access to the request factory.
         self.factory = RequestFactory()
 
-    # it doesn't work, it seems we'll have to write a test for each routes
     # python manage.py test district.tst_folder.simple.test_anonymous
-    def test_anonymous_user_access(self):
+    def test_no_params(self):
         for page in PAGES:
-            with self.subTest():
-                arguments = []
-                if page.name == 'activate':
-                    arguments = ["12", "121"]
-                request = self.factory.get(reverse(page.name, args=arguments))
-                request.user = AnonymousUser()
-                response = page.ctrl(request)
-                if page.type == "view":
+            if not page.params and page.type == "view":
+                print(f"[TEST PAGE] {page.name}")
+                with self.subTest():
+                    request = self.factory.get(reverse(page.name))
+                    request.user = AnonymousUser()
+                    response = page.ctrl(request)
                     if page.log_req:
                         self.assertEqual(response.status_code, 302)
                     else:
                         self.assertEqual(response.status_code, 200)
+
+    def test_email_change_confirm(self):
+        page = PageManager().get_page("email_change_confirm")
+
+        # test with id of user that didn't validate its email
+        with self.subTest():
+            user1 = UserDC.objects.create(username="user1", first_name="pf1", last_name="pl1", email="jsp@jsp1.com", is_active=True,
+                                          is_email_validated=False)
+
+            request = self.factory.get(reverse(page.name, kwargs={"user_id": user1.id}))
+            request.user = AnonymousUser()
+            response = page.ctrl(request, user1.id)
+            self.assertEqual(response.status_code, 200)
+
+        # test with id of user that did validate its email
+        with self.subTest():
+            user2 = UserDC.objects.create(username="user2", first_name="pf2", last_name="pl2", email="jsp@jsp2.com", is_active=True,
+                                          is_email_validated=True)
+
+            request = self.factory.get(reverse(page.name, kwargs={"user_id": user2.id}))
+            request.user = AnonymousUser()
+            response = page.ctrl(request, user2.id)
+            self.assertEqual(response.status_code, 302)
+
+        # test with random id
+        with self.subTest():
+            rand_id = 3000
+            request = self.factory.get(reverse(page.name, kwargs={"user_id": rand_id}))
+            request.user = AnonymousUser()
+            response = page.ctrl(request, rand_id)
+            self.assertEqual(response.status_code, 302)
+
