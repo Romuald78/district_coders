70310cead561e3f9d093bd1faefb4c1604651cfd
Romuald GRIGNON <romuald.grignon@gmail.com>
2022-07-07 09:23:50 +0200

    updated admin commands

--- website/district/management/commands/populate_default.py
+++ website/district/management/commands/populate_default.py
@@ -1,19 +1,15 @@
 from django.core.management import BaseCommand
-from django.db import migrations
 
-from toolbox.migration_tools.debug_data import debug_migration
+from toolbox.migration_tools.debug_data import debug_migration, createAdmin
 from toolbox.migration_tools.migration_group import group_migration
 from toolbox.migration_tools.migration_inspect_mode import mode_migration
 from toolbox.migration_tools.migration_language import language_migration
 
 
 class Command(BaseCommand):
+
     def handle(self, *args, **options):
+        # Create default languages,  inspector modes and groups
         language_migration()
         mode_migration()
         group_migration()
-        debug_migration()
-        # migrations.RunPython(language_migration)
-        # migrations.RunPython(mode_migration)
-        # migrations.RunPython(group_migration)
-        # migrations.RunPython(debug_migration)
