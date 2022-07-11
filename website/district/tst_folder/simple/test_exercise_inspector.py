9e223ec60a437645440ed93774a0fe5dc5410953
CYTech Student <vincent.ky09@gmail.com>
2022-07-11 17:11:06 +0200

    finishing test_user_access + making other tests in test_exercise_inspector. Test are only runing on a certain lanuage for the moment

--- website/district/tst_folder/simple/test_exercise_inspector.py
+++ website/district/tst_folder/simple/test_exercise_inspector.py
@@ -1,4 +1,6 @@
 import json
+import os.path
+from datetime import timedelta
 
 from django.core import management
 from django.test import TransactionTestCase, RequestFactory
@@ -9,11 +11,20 @@ from district.controllers.ctrl_exercise import ctrl_json_exercise_inspect, ctrl_
 from district.models.assessment import Assessment
 from district.models.exo2test import Exo2Test
 from district.models.exotest2lang import ExoTest2Lang
+from district.models.group import GroupDC
+from district.models.language import Language
+from district.models.test import TestDC
 from district.models.testresult import TestResult
 from district.models.user import UserDC
+from website.settings import MEDIA_ROOT
 
 
 class ExerciseInspectorTest(TransactionTestCase):
+
+    def __init__(self, methodName='test_user_access'):
+        self.lang = ["C", "JS", "PHP"]
+        super().__init__(methodName)
+
     def setUp(self):
         # Every test needs access to the request factory.
         self.factory = RequestFactory()
@@ -31,12 +42,55 @@ class ExerciseInspectorTest(TransactionTestCase):
             solve_percentage=100,
             assessment_id=4)
 
+        # create an assessment, and solve it totally
+        month = timedelta(days=30)
+        today = timezone.now()
+        my_test = TestDC.objects.get(title="Test_#4")
+        my_asse = Assessment.objects.create(
+            start_time=today - month,
+            end_time=today + month,
+            training_time=today + month * 2,
+            test_id=my_test.id
+        )
+
+        my_group = GroupDC.objects.get(name="Group #2")
+        my_asse.groups.add(my_group)
+
+        for ex2tst in my_test.exo2test_set.all():
+            search_valid_lang = True
+            for language in self.lang:
+                if search_valid_lang:
+                    extst2lang = ex2tst.exotest2lang_set.filter(lang__name=language)
+                    if len(extst2lang.all()) > 0:
+                        search_valid_lang = False
+                        TestResult.objects.create(
+                            exo_test2lang=extst2lang.first(),
+                            user=user,
+                            solve_code=exotst2lang.lang.default_code,
+                            solve_percentage=100,
+                            assessment_id=my_asse.id)
 
     # python manage.py test district.tst_folder.simple.test_exercise_inspector.ExerciseInspectorTest.test_user_access -v 2 --failfast
     def test_user_access(self):
-        for curr_user in UserDC.objects.all():
+        for asse_item in Assessment.objects.all():
+            # check the rank
+            # getting max rank of complete exercise
+            min_rank_todo = asse_item.test.exo2test_set.order_by("rank").first().rank
+            min_search = True
+            is_test_complete = True
+            for asse_ex2tst in asse_item.test.exo2test_set.order_by("-rank"):
+                if min_search:
+                    extst2lng_all = asse_ex2tst.exotest2lang_set.order_by("-testresult__solve_percentage")
+                    if len(extst2lng_all.all()) > 0:
+                        extst2lng = extst2lng_all.first()
+                        if len(extst2lng.testresult_set.all()) == 0 \
+                                or extst2lng.testresult_set.first().solve_percentage < asse_ex2tst.solve_percentage_req:
+                            is_test_complete = False
+                            min_rank_todo = asse_ex2tst.rank
+                        else:
+                            min_search = False
             for ex2tst_item in Exo2Test.objects.all():
-                for asse_item in Assessment.objects.all():
+                for curr_user in UserDC.objects.all():
                     asse_qs = Assessment.objects.filter(test__exo2test=ex2tst_item, groups__userdc=curr_user, id=asse_item.id)
                     do_access = len(asse_qs.all()) > 0
                     do_see = len(asse_qs.all()) > 0
@@ -47,17 +101,7 @@ class ExerciseInspectorTest(TransactionTestCase):
                         if len(other_asse.filter(start_time__lte=timezone.now(), end_time__gt=timezone.now()).all()) > 0:
                             # check if the current assessment is in process
                             if asse_item.start_time.__le__(timezone.now()) and asse_item.end_time.__gt__(timezone.now()):
-                                # check the rank
-                                # getting max rank of complete exercise
-                                min_rank_todo = asse_item.test.exo2test_set.order_by("rank").first().rank
-                                for asse_ex2tst in asse_item.test.exo2test_set.order_by("-rank"):
-                                    extst2lng_all = asse_ex2tst.exotest2lang_set.order_by("-testresult__solve_percentage")
-                                    if len(extst2lng_all.all()) > 0:
-                                        extst2lng = extst2lng_all.first()
-                                        if len(extst2lng.testresult_set.all()) > 0 \
-                                                and extst2lng.testresult_set.first().solve_percentage <= asse_ex2tst.solve_percentage_req:
-                                            min_rank_todo = asse_ex2tst.rank
-                                if ex2tst_item.rank != min_rank_todo:
+                                if ex2tst_item.rank != min_rank_todo or is_test_complete:
                                     do_access = False
                                     err_msg = "Rank not valid"
                             else:
@@ -91,7 +135,7 @@ class ExerciseInspectorTest(TransactionTestCase):
                         err_msg = "bad group or something"
 
                     # Test part
-                    print(f"[user:{curr_user.id}][ex2tst:{ex2tst_item.id}][asse:{asse_item.id}] : {err_msg}")
+                    # print(f"[asse:{asse_item.id}][ex2tst:{ex2tst_item.id}][user:{curr_user.id}] : {err_msg}")
                     # test on exercise_details
                     with self.subTest():
                         request = self.factory.get(reverse('exercise_details'),
@@ -114,9 +158,10 @@ class ExerciseInspectorTest(TransactionTestCase):
                             self.assertEqual(response.status_code, 302)
                     # test on exercise_inspect
                     with self.subTest():
-                        for extst2lng in ex2tst_item.exotest2lang_set.filter(
-                                lang__name__in=["C", "JS", "PHP"]).all():
-                            lang = extst2lng.lang
+                        for lang in Language.objects.filter(name__in=self.lang).all():
+                            lang_access = True
+                            if len(ex2tst_item.exotest2lang_set.filter(lang__name=lang.name).all()) == 0:
+                                lang_access = False
                             # print(f"--> {lang.name}")
                             request = self.factory.post(reverse('exercise_inspect'),
                                                         {
@@ -127,27 +172,165 @@ class ExerciseInspectorTest(TransactionTestCase):
                             request.user = curr_user
                             response = ctrl_json_exercise_inspect(request)
                             dict_json = json.loads(response.content)
-                            # print(dict_json)
-                            # if do_access:
-                            #     self.assertLessEqual(dict_json["exit_code"], 0)
-                            # else:
-                            #     self.assertGreater(dict_json["exit_code"], 0)
-                            self.assertNotEqual(dict_json["exit_code"], 0)
+                            if do_access and lang_access:
+                                self.assertNotIn("err_msg", dict_json)
+                            else:
+                                self.assertIn("err_msg", dict_json)
 
+    # TODO only works for C script
     def test_OK(self):
-        pass
+        # getting the user
+        user = UserDC.objects.get(username="user_4")
+        extst = Exo2Test.objects.get(id=33)
+        asse = Assessment.objects.get(id=9)
+        lang = Language.objects.get(name="C")
+        with open(os.path.join(MEDIA_ROOT, "..", "..", "tmp", "user001.c"), "r") as f:
+            raw_code = f.read()
+
+        request = self.factory.post(reverse('exercise_inspect'),
+                                    {
+                                        "ex2tst_id": extst.id,
+                                        "asse_id": asse.id,
+                                        "lang_id": lang.id,
+                                        "raw_code": raw_code})
+        request.user = user
+        response = ctrl_json_exercise_inspect(request)
+        dict_json = json.loads(response.content)
+        with self.subTest():
+            self.assertNotIn("err_msg", dict_json)
+        with self.subTest():
+            self.assertEqual(dict_json["exit_code"], 0)
 
     def test_NOK(self):
-        pass
+        # getting the user
+        user = UserDC.objects.get(username="user_4")
+        extst = Exo2Test.objects.get(id=33)
+        asse = Assessment.objects.get(id=9)
+        lang = Language.objects.get(name="C")
+        with open(os.path.join(MEDIA_ROOT, "..", "..", "tmp", "user001 - wrong.c"), "r") as f:
+            raw_code = f.read()
+
+        request = self.factory.post(reverse('exercise_inspect'),
+                                    {
+                                        "ex2tst_id": extst.id,
+                                        "asse_id": asse.id,
+                                        "lang_id": lang.id,
+                                        "raw_code": raw_code})
+        request.user = user
+        response = ctrl_json_exercise_inspect(request)
+        dict_json = json.loads(response.content)
+        with self.subTest():
+            self.assertNotIn("err_msg", dict_json)
+        with self.subTest():
+            self.assertEqual(dict_json["exit_code"], 1)
 
     def test_empty_code(self):
-        pass
+        # getting the user
+        user = UserDC.objects.get(username="user_4")
+        extst = Exo2Test.objects.get(id=33)
+        asse = Assessment.objects.get(id=9)
+        lang = Language.objects.get(name="C")
+        raw_code = ""
+
+        request = self.factory.post(reverse('exercise_inspect'),
+                                    {
+                                        "ex2tst_id": extst.id,
+                                        "asse_id": asse.id,
+                                        "lang_id": lang.id,
+                                        "raw_code": raw_code})
+        request.user = user
+        response = ctrl_json_exercise_inspect(request)
+        dict_json = json.loads(response.content)
+        with self.subTest():
+            self.assertNotIn("err_msg", dict_json)
+        with self.subTest():
+            self.assertEqual(dict_json["exit_code"], 1)
 
     def test_syntax_error(self):
-        pass
+        # getting the user
+        user = UserDC.objects.get(username="user_4")
+        extst = Exo2Test.objects.get(id=33)
+        asse = Assessment.objects.get(id=9)
+        lang = Language.objects.get(name="C")
+        with open(os.path.join(MEDIA_ROOT, "..", "..", "tmp", "user001 - syntax.c"), "r") as f:
+            raw_code = f.read()
+
+        request = self.factory.post(reverse('exercise_inspect'),
+                                    {
+                                        "ex2tst_id": extst.id,
+                                        "asse_id": asse.id,
+                                        "lang_id": lang.id,
+                                        "raw_code": raw_code})
+        request.user = user
+        response = ctrl_json_exercise_inspect(request)
+        dict_json = json.loads(response.content)
+        with self.subTest():
+            self.assertNotIn("err_msg", dict_json)
+        with self.subTest():
+            self.assertEqual(dict_json["exit_code"], 1)
 
     def test_infinite_loop(self):
-        pass
+        # getting the user
+        user = UserDC.objects.get(username="user_4")
+        extst = Exo2Test.objects.get(id=33)
+        asse = Assessment.objects.get(id=9)
+        lang = Language.objects.get(name="C")
+        with open(os.path.join(MEDIA_ROOT, "..", "..", "tmp", "user001 - infLoop.c"), "r") as f:
+            raw_code = f.read()
+
+        request = self.factory.post(reverse('exercise_inspect'),
+                                    {
+                                        "ex2tst_id": extst.id,
+                                        "asse_id": asse.id,
+                                        "lang_id": lang.id,
+                                        "raw_code": raw_code})
+        request.user = user
+        response = ctrl_json_exercise_inspect(request)
+        dict_json = json.loads(response.content)
+        with self.subTest():
+            self.assertNotIn("err_msg", dict_json)
+        with self.subTest():
+            self.assertEqual(dict_json["exit_code"], 1)
 
     def test_code_too_long(self):
-        pass
+        # getting the user
+        user = UserDC.objects.get(username="user_4")
+        extst = Exo2Test.objects.get(id=33)
+        asse = Assessment.objects.get(id=9)
+        lang = Language.objects.get(name="C")
+
+        # at the limit size
+        with open(os.path.join(MEDIA_ROOT, "..", "..", "tmp", "user001 - almostTooLong.c"), "r") as f:
+            raw_code = f.read()
+
+        request = self.factory.post(reverse('exercise_inspect'),
+                                    {
+                                        "ex2tst_id": extst.id,
+                                        "asse_id": asse.id,
+                                        "lang_id": lang.id,
+                                        "raw_code": raw_code})
+        request.user = user
+        response = ctrl_json_exercise_inspect(request)
+        dict_json = json.loads(response.content)
+        with self.subTest():
+            self.assertNotIn("err_msg", dict_json)
+        with self.subTest():
+            self.assertEqual(dict_json["exit_code"], 0)
+
+        # beyond the limit size
+        with open(os.path.join(MEDIA_ROOT, "..", "..", "tmp", "user001 - tooLong.c"), "r") as f:
+            raw_code = f.read()
+
+        request = self.factory.post(reverse('exercise_inspect'),
+                                    {
+                                        "ex2tst_id": extst.id,
+                                        "asse_id": asse.id,
+                                        "lang_id": lang.id,
+                                        "raw_code": raw_code})
+        request.user = user
+        response = ctrl_json_exercise_inspect(request)
+        dict_json = json.loads(response.content)
+        with self.subTest():
+            self.assertIn("err_msg", dict_json)
+        with self.subTest():
+            self.assertEqual(dict_json["exit_code"], 3)
