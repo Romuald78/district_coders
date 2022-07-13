7cb6ce3b6b148fb67688d17f316fafd1aec74f51
CYTech Student <vincent.ky09@gmail.com>
2022-07-13 17:14:26 +0200

    doing test_json_group_register for test_anonymous and test_user_connect (not working)

--- website/district/tst_folder/users/test_user_connect.py
+++ website/district/tst_folder/users/test_user_connect.py
@@ -1,6 +1,11 @@
+import json
+
+from django.contrib.auth.models import AnonymousUser
 from django.core import management
 from django.test import TransactionTestCase, RequestFactory, Client
 
+from district.models.group import GroupDC
+from district.models.user import UserDC
 from toolbox.utils.route_mgr import PageManager
 
 
@@ -26,3 +31,45 @@ class UserConnectTest(TransactionTestCase):
         # If connection ok, redirect to profile page
         self.assertRedirects(response, PageManager().get_URL("profile"))
 
+    # TODO : not working
+    def test_json_group_register(self):
+        page = PageManager().get_page('group_register')
+        for user in UserDC.objects.filter(id__gt=0).all():
+            for group in GroupDC.objects.all():
+                request = self.factory.post(page.url, {"register_key": group.register_key})
+                request.user = user
+                response = page.ctrl(request)
+                dict_json = json.loads(response.content)
+                print(f"[user: {user.username}][group: {group.id}]")
+                print("groups:", GroupDC.objects.filter(id=group.id, userdc=user.id).all())
+                # district.GroupDC.None -> not a group but counted as one
+                if len(GroupDC.objects.filter(id=group.id, userdc=user.id).all()) != 0:
+                    with self.subTest():
+                        self.assertEqual(dict_json["exit_code"], 9)
+                    with self.subTest():
+                        self.assertIn("err_msg", dict_json)
+                else:
+                    with self.subTest():
+                        self.assertEqual(dict_json["exit_code"], 0)
+                    with self.subTest():
+                        self.assertNotIn("err_msg", dict_json)
+
+            # test empty key
+            request = self.factory.post(page.url, {"register_key": ""})
+            request.user = user
+            response = page.ctrl(request)
+            dict_json = json.loads(response.content)
+            with self.subTest():
+                self.assertEqual(dict_json["exit_code"], 2)
+            with self.subTest():
+                self.assertIn("err_msg", dict_json)
+
+            # test key which doesn't exist
+            request = self.factory.post(page.url, {"register_key": "Tu lis mon code ? Bonne chance"})
+            request.user = user
+            response = page.ctrl(request)
+            dict_json = json.loads(response.content)
+            with self.subTest():
+                self.assertEqual(dict_json["exit_code"], 1)
+            with self.subTest():
+                self.assertIn("err_msg", dict_json)
