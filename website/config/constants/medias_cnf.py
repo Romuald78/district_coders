ffe4287c2d49990de96a7e4063ddb1731fcd6d4e
Romuald GRIGNON <romuald.grignon@gmail.com>
2024-06-01 15:20:56 +0200

    update parameters

--- website/config/constants/medias_cnf.py
+++ website/config/constants/medias_cnf.py
@@ -1,10 +1,10 @@
 # Media ROOT (for back end access)
 # For Windows / Vincent Computer
-medias_absolute_path = 'h:\Documents\WORK\district_coders\district_coders\website\medias'
+#medias_absolute_path = 'h:\Documents\WORK\district_coders\district_coders\website\medias'
 
 # Media ROOT (for back end access)
 # For Linux / Romuald Computer
-# medias_absolute_path = '/home/cytech/district_coders/website/medias'
+medias_absolute_path = '/home/cytech/district_coders/website/medias'
 
 # Media URL (for web access)
 medias_url_root = '/medias/'
