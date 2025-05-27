69fafedb01ca0f6e2c5557829f11679acaba6fbd
Inès <charines@outlook.fr>
2025-05-27 18:11:26 +0200

    Update front assessments dynamic colours & conditional button & improved HTML/Tailwind readability

--- website/website/settings.py
+++ website/website/settings.py
@@ -114,7 +114,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
-LANGUAGE_CODE = 'fr-fr'
+LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
