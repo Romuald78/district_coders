ba76b71c7bbf731b89e377462b20ddf7642d8c95
Romuald GRIGNON <romuald.grignon@gmail.com>
2025-06-13 12:50:55 +0200

    use email_cnf_local

---
+++ website/config/secure/email_cnf_local.py
@@ -0,0 +1,9 @@
+EMAIL_HOST          = ""
+EMAIL_PORT          = 587
+EMAIL_HOST_USER     = ""
+EMAIL_HOST_PASSWORD = ""
+EMAIL_USE_TLS       = True
+# EMAIL_USE_SSL       = False
+# EMAIL_TIMEOUT = ""
+# EMAIL_SSL_KEYFILE = ""
+# EMAIL_SSL_CERTFILE = ""
