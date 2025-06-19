d5bf269116db75aba9f21430710ff35c34be540e
Inès <charines@outlook.fr>
2025-06-19 16:30:36 +0200

    Add mandatory size to be respected

--- website/toolbox/users/signup.py
+++ website/toolbox/users/signup.py
@@ -1,6 +1,7 @@
 # authentication/forms.py
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
+from django.core.exceptions import ValidationError
 from django.db import models
 
 from district.models.user import UserDC
@@ -24,3 +25,15 @@ class SignupForm(UserCreationForm):
         email = cleaned_data.get('email')
         if email is None or len(email) == 0:
             self.add_error('email', 'Empty email')
+
+    def clean_icon(self):
+        icon = self.cleaned_data.get('icon')
+        if icon:
+            from PIL import Image
+            img = Image.open(icon)
+            max_width = 500
+            max_height = 500
+            if img.width > max_width or img.height > max_height:
+                raise ValidationError(
+                    f"The image is too large ({img.width}x{img.height}px). Maximum dimensions allowed: {max_width}x{max_height}px.")
+        return icon
--- website/toolbox/users/update.py
+++ website/toolbox/users/update.py
@@ -3,7 +3,7 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 from django.db import models
 from django import forms
-
+from django.core.exceptions import ValidationError
 from district.models.user import UserDC
 
 
@@ -13,6 +13,19 @@ class UserUpdateForm(forms.ModelForm):
         # self.fields['username'].disabled = True
         self.fields['email'].disabled = True
 
+    def clean_icon(self):
+        icon = self.cleaned_data.get('icon')
+        if icon:
+            from PIL import Image
+            img = Image.open(icon)
+            max_width = 500
+            max_height = 500
+            if img.width > max_width or img.height > max_height:
+                raise ValidationError(
+                    f"The image is too large ({img.width}x{img.height}px). Maximum dimensions allowed: {max_width}x{max_height}px.")
+        return icon
+
     class Meta(forms.ModelForm):
         model = UserDC
         fields = ('username', 'first_name', 'last_name', 'email', 'icon', 'description')
+
