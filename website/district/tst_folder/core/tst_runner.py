1b2c861b920ab7a2f703803432f5cf4e2f2980ca
Inès <charines@outlook.fr>
2025-07-01 12:54:52 +0200

    Update test runner to run all test in one file

--- website/district/tst_folder/core/tst_runner.py
+++ website/district/tst_folder/core/tst_runner.py
@@ -1,8 +1,10 @@
+import os
 import unittest
 
 from django.test.runner import DiscoverRunner
 
 from district.tst_folder.core.tst_loader import DistrictTestLoader
+from website.settings import BASE_DIR
 
 
 class DistrictTestRunner(DiscoverRunner):
@@ -13,7 +15,12 @@ class DistrictTestRunner(DiscoverRunner):
         if test_labels:
             suite = unittest.TestSuite()
             for label in test_labels:
-                suite.addTests(loader.loadTestsFromName(label))
+                path = os.path.join(BASE_DIR,label.replace(".", "/"))
+                if os.path.isdir(path) :
+                    suite.addTests(loader.discover(label))
+                else :
+                    suite.addTests(loader.loadTestsFromName(label))
+
         else:
             suite = loader.discover(start_dir='district')
 
